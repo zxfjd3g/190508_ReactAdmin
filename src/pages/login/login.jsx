d5f477360f4acdc910df65661021e147375738bb
zxfjd3g <258147149@qq.com>
2019-08-30 16:27:54 +0800

    login from

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import { Form, Icon, Input, Button } from 'antd';
 import logo from './images/logo.png'
 import './login.less'
+
+const {Item} = Form
 
 /* 
 一级路由组件: 登陆
 */
 export default class Login extends Component {
+
+  handleSubmit = event => {
+    // 阻止事件默认行为(不提交表单)
+    event.preventDefault();
+    alert('发送登陆的ajax登陆')
+  };
+
   render() {
     return (
       <div className="login">
@@ -15,7 +25,26 @@ export default class Login extends Component {
         </div>
         <div className="login-content">
           <h1>用户登陆</h1>
-          <div>登陆form</div>
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <Item>
+              <Input
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }}/>}
+                placeholder="用户名"
+              />
+            </Item>
+            <Form.Item>
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="password"
+                placeholder="密码"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                登 陆
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
       </div>
     )
