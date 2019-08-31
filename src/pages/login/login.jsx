67aeb4a3139cfc29665d45c94091055ac23782af
zxfjd3g <258147149@qq.com>
2019-08-31 09:34:53 +0800

    login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -8,15 +8,24 @@ const {Item} = Form
 /* 
 一级路由组件: 登陆
 */
-export default class Login extends Component {
+class Login extends Component {
 
   handleSubmit = event => {
     // 阻止事件默认行为(不提交表单)
     event.preventDefault();
+    // 得到输入数据
+    const form = this.props.form
+    const username = form.getFieldValue('username')
+    const password = form.getFieldValue('password')
+    const values = form.getFieldsValue()
+    console.log(username, password, values)
+
     alert('发送登陆的ajax登陆')
   };
 
   render() {
+    const form = this.props.form
+    const getFieldDecorator  = this.props.form.getFieldDecorator
     return (
       <div className="login">
         <div className="login-header">
@@ -27,17 +36,28 @@ export default class Login extends Component {
           <h1>用户登陆</h1>
           <Form onSubmit={this.handleSubmit} className="login-form">
             <Item>
-              <Input
-                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }}/>}
-                placeholder="用户名"
-              />
+              {
+                getFieldDecorator('username', {})(
+                  <Input
+                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }}/>}
+                    placeholder="用户名"
+                  />
+                )
+              }
+
+              
             </Item>
             <Form.Item>
-              <Input
-                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type="password"
-                placeholder="密码"
-              />
+              {
+                getFieldDecorator('password', {})(
+                  <Input
+                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    type="password"
+                    placeholder="密码"
+                  />
+                )
+              }
+              
             </Form.Item>
             <Form.Item>
               <Button type="primary" htmlType="submit" className="login-form-button">
@@ -50,3 +70,38 @@ export default class Login extends Component {
     )
   }
 }
+
+const WrappedLogin = Form.create()(Login)
+export default WrappedLogin
+
+/* 
+Form组件: 包含有<Form>的组件  ==> Login
+
+function (props) {
+  // 封装包含n个功能方法的对象: form
+  // 将form传递给要包装的组件
+
+  return (
+    <Login {...props} form={form对象}></Login>
+  )
+}
+
+高阶函数
+  特点
+      接收参数是函数
+      返回值是函数
+  常见:
+      Promise()/then()
+      数组很多方法: map()/reduce()/find()/filter()...
+      setTimeout()/setInterval()
+      addEventListner()
+      bind()
+      Form.create()()
+  好处
+      功能更加动态, 更加具有扩展
+高阶组件
+  参数为组件，返回值为新组件的函数
+  React 中用于复用组件逻辑的一种高级技巧
+    将多个组件通用的功能提取到高阶组件函数中, 再通过标签属性传递要包装的组件
+
+*/
