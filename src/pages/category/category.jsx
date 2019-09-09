6ae9d4db4f772fd99d2553f8710754ad682e7180
zxfjd3g <258147149@qq.com>
2019-09-09 11:55:35 +0800

    hooks

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import {Modal, message} from 'antd'
 
 import CategoryForm from './category-form'
@@ -14,53 +14,63 @@ import {
 /**
  * 分类管理
  */
-export default class Category extends Component {
+export default function Category (props) {
 
-  state = {
-    categorys: [], // 所有分类的数组
-    loading: false, // 标识是否正在请求中
-    showStatus: 0, // 0: 都不显示, 1: 显示添加 2: 显示修改
-  }
+  const [categorys, setCategorys] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [showStatus, setShowStatus] = useState(0)
+
+  const formRef = useRef()
+  const categoryRef = useRef({})
+  const columnsRef = useRef([
+    {
+      title: '分类名称',
+      dataIndex: 'name',
+    },
+    {
+      width: 250,
+      title: '操作',
+      render: (category) => <LinkButton onClick={() => showUpdate(category)}>修改分类</LinkButton>
+    },
+  ])
+
+  useEffect(() => {
+    getCategorys()
+  }, [])
+
+
 
   /* 
   获取获取所有分类列表显示
   */
-  getCategorys = async () => {
+  async function getCategorys () {
     // 显示loading
-    this.setState({
-      loading: true
-    })
+    setLoading(true)
     const result = await reqCategorys()
     // 隐藏loading
-    this.setState({
-      loading: false
-    })
+    setLoading(false)
     if (result.status===0) {
       const categorys = result.data
-      this.setState({
-        categorys
-      })
+      setCategorys(categorys)
     }
   }
 
   /* 
   添加分类
   */
-  addCategory = () => {
+  function addCategory () {
     // 对form进行验证
-    this.form.validateFields(async (error, values) => {
+    formRef.current.validateFields(async (error, values) => {
       if (!error) {
         // 重置输入框中的数据(变为initialValue)
-        this.form.resetFields()
+        formRef.current.resetFields()
         // 验证通过后发请求添加分类
         const result = await reqAddCategory(values.categoryName)
         if (result.status===0) {
-          this.setState({
-            showStatus: 0
-          })
+          setShowStatus(0)
           message.success('添加分类成功')
           // 获取最新分类列表显示
-          this.getCategorys()
+          getCategorys()
         } else {
           message.error(result.msg || '添加分类失败')
         }
@@ -72,22 +82,20 @@ export default class Category extends Component {
   /* 
   修改分类
   */
-  UpdateCategory = () => {
+  function updateCategory () {
     // 对form进行验证
-    this.form.validateFields(async (error, values) => {
+    formRef.current.validateFields(async (error, values) => {
       if (!error) {
         // 重置输入框中的数据(变为initialValue)
-        this.form.resetFields()
+        formRef.current.resetFields()
         // 验证通过后发请求更新分类
-        values.categoryId = this.category._id
+        values.categoryId = categoryRef.current._id
         const result = await reqUpdateCategory(values)
         if (result.status===0) {
-          this.setState({
-            showStatus: 0
-          })
+          setShowStatus(0)
           message.success('修改分类成功')
           // 获取最新分类列表显示
-          this.getCategorys()
+          getCategorys()
         } else {
           message.error(result.msg || '修改分类失败')
         }
@@ -98,94 +106,67 @@ export default class Category extends Component {
   /* 
   取消
   */
-  handleCancel = () => {
+  function handleCancel () {
     // 重置输入框中的数据(变为initialValue)
-    this.form.resetFields()
-    this.setState({
-      showStatus: 0
-    })
+    formRef.current.resetFields()
+    setShowStatus(0)
   }
 
   /* 
   显示添加界面
   */
-  showAdd = () => {
-    this.setState({
-      showStatus: 1
-    })
+  function showAdd () {
+    setShowStatus(1)
   }
   /* 
   显示修改界面
   */
-  showUpdate = (category) => {
+  function showUpdate (category) {
     // 保存当前分类
-    this.category = category
-
-    this.setState({
-      showStatus: 2
-    })
+    categoryRef.current = category
+    setShowStatus(2)
   }
 
-  componentDidMount () {
-    this.getCategorys()
-  }
+  // 取出保存的用于更新的分类对象
+  const category = categoryRef.current
 
-  componentWillMount () {
-    this.columns = [
-      {
-        title: '分类名称',
-        dataIndex: 'name',
-      },
-      {
-        width: 250,
-        title: '操作',
-        render: (category) => <LinkButton onClick={() => this.showUpdate(category)}>修改分类</LinkButton>
-      },
-    ]
-  }
+  
+  const extra = (
+    <Button type="primary" onClick={showAdd}>
+      <Icon type="plus"></Icon>
+      添加
+    </Button>
+  )
 
-  render() {
-    // 取出保存的用于更新的分类对象
-    const category = this.category || {}
-
-    const {categorys, loading, showStatus} = this.state
-    const extra = (
-      <Button type="primary" onClick={this.showAdd}>
-        <Icon type="plus"></Icon>
-        添加
-      </Button>
-    )
-
-    return (
-      <Card extra={extra}>
-        <Table
-          loading={loading}
-          bordered={true}
-          rowKey="_id"  /* 将数据对象category的_id的属性值作为每行的key */
-          pagination={{pageSize: 2, showQuickJumper: true}}
-          dataSource={categorys} 
-          columns={this.columns} 
+  return (
+    <Card extra={extra}>
+      <Table
+        loading={loading}
+        bordered={true}
+        rowKey="_id"  /* 将数据对象category的_id的属性值作为每行的key */
+        pagination={{pageSize: 2, showQuickJumper: true}}
+        dataSource={categorys} 
+        columns={columnsRef.current} 
+      />
+      <Modal
+        title="添加分类"
+        visible={showStatus===1}
+        onOk={addCategory}
+        onCancel={handleCancel}
+      >
+        <CategoryForm setForm={(form) => formRef.current = form}/>
+      </Modal>
+      <Modal
+        title="修改分类"
+        visible={showStatus===2}
+        onOk={updateCategory}
+        onCancel={handleCancel}
+      >
+        <CategoryForm 
+          categoryName={category.name} 
+          setForm={(form) => formRef.current = form}
         />
-        <Modal
-          title="添加分类"
-          visible={showStatus===1}
-          onOk={this.addCategory}
-          onCancel={this.handleCancel}
-        >
-          <CategoryForm setForm={(form) => this.form = form}/>
-        </Modal>
-        <Modal
-          title="修改分类"
-          visible={showStatus===2}
-          onOk={this.UpdateCategory}
-          onCancel={this.handleCancel}
-        >
-          <CategoryForm 
-            categoryName={category.name} 
-            setForm={(form) => this.form = form}
-          />
-        </Modal>
-      </Card>
-    )
-  }
+      </Modal>
+    </Card>
+  )
 }
